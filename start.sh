a1f84c36be78182df65f87fe71a159e2b0f5b103
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-09 12:59:56 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ZauteKm/File-Store-Bot.git /File-Store-Bot
+cd /File-Store-Bot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py

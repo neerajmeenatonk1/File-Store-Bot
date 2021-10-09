923f50967d3d6f76e65d1c3bdb9fcebb8f9e309c
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-09 12:49:23 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+class Config(object):
+	API_ID = int(os.environ.get("API_ID"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+	DATABASE_URL = os.environ.get("DATABASE_URL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	ABOUT_BOT_TEXT = f"""
+This is Permanent Files Store Bot!
+Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+
+🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
+📝 **Language:** [Python3](https://www.python.org)
+📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+📡 **Follow me:** [GitHub](https://github.com/ZauteKm)
+🧑🏻‍💻 **Developer:** [@ZauteKm](https://t.me/ZauteKm)
+🤖 **Bot Lists:** [Click here](https://t.me/BotzListBot)
+🌀 **Source Code:** [GitHub](http://github.com/ZauteKm/File-Store-Bot)
+📢 **Channel:** [Subscribe Now](https://t.me/TGBotsProJect)
+"""
+	ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **Developer:** @ZauteKm
+
+Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
+
+Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
+
+> Please ❤️ Contact [Donate Now](https://t.me/zautebot)
+"""
+	HOME_TEXT = """
+Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
+
+Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+"""
